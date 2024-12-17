8a18f4fb685bbf99d760aafab006d83bee506746
NielsRed <1unclejoe9@gmail.com>
2024-12-17 11:36:03 +0100

    Update Userinpit.h
    Adding defines instead of random values

--- Core/Inc/Front Layer/UserInput.h
+++ Core/Inc/Front Layer/UserInput.h
@@ -10,6 +10,12 @@
 #ifndef INC_FRONT_LAYER_USERINPUT_H_
 #define INC_FRONT_LAYER_USERINPUT_H_
 
+#define MAX_BUFFER_SIZE 1023
+
+#define DATALENGTH_ERR -3
+#define UART_ERR -2
+#define USER_INPUT_ERR -1
+
 
 char FL_Parse_String(uint16_t size, char String_Received[]);
 #endif /* INC_FRONT_LAYER_USERINPUT_H_ */
